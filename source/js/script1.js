dba24cf7afa49a2a311f396dd4be02db80e8191a
Artem Riabokon <artemriabokon17@gmail.com>
2022-06-20 21:38:20 +0300

    Commit

--- source/js/script1.js
+++ source/js/script1.js
@@ -47,3 +47,96 @@ function showSlide(x) {
 function changeImage(id, way) {
 	document.getElementById(id).src = way;
 }
+
+/*PopUp*/
+const popupLinks = document.querySelectorAll(".popup-link")
+const body = document.querySelector("body")
+const popupCloseIcon = document.querySelectorAll(".close-popup")
+const form = document.querySelector('popup-form__form')
+const timeOut = 800;
+
+let unlock = true;
+
+if (popupLinks.length > 0) {
+	for (let i = 0; i < popupLinks.length; i++) {
+		const popupLink = popupLinks[i];
+		popupLink.addEventListener("click", function (e) {
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const curentPopup = document.getElementById(popupName);
+			popupOpen(curentPopup);
+			e.preventDefault();
+		})
+	}
+
+}
+if (popupCloseIcon.length > 0) {
+	for (let i = 0; i < popupCloseIcon.length; i++) {
+		const el = popupCloseIcon[i];
+		el.addEventListener("click", function (e) {
+			popupClose(el.closest(".popup-form"))
+			e.preventDefault();
+
+		})
+	}
+}
+
+
+function popupOpen(curentPopup) {
+	if (curentPopup && unlock) {
+		const popupActive = document.querySelector(".popup-form.open");
+		if (popupActive) {
+			popupClose(popupActive, false)
+		}
+		else {
+			bodyLock();
+		}
+		debugger
+		curentPopup.classList.add("open");
+		curentPopup.addEventListener("click", function (e) {
+			if (!e.target.closest(".popup-form__content")) {
+				popupClose(e.target.closest(".popup-form"));
+			}
+		})
+	}
+
+}
+function popupClose(popupActive, doUnlock = true) {
+	if (unlock) {
+		popupActive.classList.remove("open")
+
+		if (doUnlock) {
+			bodyUnLock();
+		}
+	}
+	debugger
+}
+
+function bodyLock() {
+	const lockPaddingValue = (window.innerWidth - document.querySelector('.wrapper').offsetWidth) + 'px';
+	console.log(body);
+	body.style.paddingRight = lockPaddingValue;
+	body.classList.add('lock');
+	unlock = false;
+	setTimeout(function () {
+		unlock = true;
+	}, timeOut);
+}
+
+function bodyUnLock() {
+	setTimeout(function () {
+		body.style.paddingRight = '0px';
+		body.classList.remove('lock')
+	}, timeOut)
+}
+
+function submitForm() {
+	const frm = document.querySelector('.popup-form__form');
+	frm.reset();
+	debugger;
+	popupOpen(popup2);
+	
+	return false;
+
+}
+/*PopUp*/
+
